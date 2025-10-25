78dad1cf5f9bf5bacbdb745f6b6acd5381bfe649
Keerthan N S <keerthanns091@gmail.com>
2025-10-25 23:40:49 +0530

    [feat] marquee for projects [add] socials card

--- components/v3/ProjectsCard.tsx
+++ components/v3/ProjectsCard.tsx
@@ -9,12 +9,13 @@ import { experiences, projects } from "@/data";
 import Link from "next/link";
 import { BsStack } from "react-icons/bs";
 import { GrProjects } from "react-icons/gr";
+import { Marquee } from "../ui/marquee";
 
 export default function ProjectsCard() {
   return (
     <Fragment>
       {/* <div className="xl:col-span-2 xl:row-span-2 xl:col-start-6 xl:row-start-5 rounded-2xl border-2 border-light-text/10"> */}
-      <div className="xl:col-span-2 xl:row-span-3 xl:col-start-6 rounded-2xl border-2 border-light-text/10">
+      <div className="hidden md:block xl:col-span-2 xl:row-span-3 xl:col-start-6 rounded-2xl border-2 border-light-text/10">
         <div className="pt-4 pb-2 relative">
           <div className="border-b-2 border-light-text/10 pb-2">
             <IconBadge
@@ -25,44 +26,61 @@ export default function ProjectsCard() {
             />
             <p className="text-sm text-neutral-400">Works Gallery</p>
           </div>
-          {/* md:h-24 */}
-          <div className="grid grid-cols-2 gap-4 px-4 pt-4 md:flex overflow-x-auto whitespace-nowrap md:h-44 scrollbar-hide">
-            {
-              projects.slice(0, 4).map((project, index) => (
-                <div key={index} className="md:inline-block">
-                  <a href={project.link ? project.link : project.github} target="_blank" rel="noopener noreferrer" aria-label={`${project.title}`}
-                    className="block rounded-2xl w-full h-28 md:w-44 md:h-32 overflow-hidden hover:scale-105 hover:shadow-lg hover:border-theme-primary transition-transform duration-300">
-                    <img src={project.img} className="object-cover h-full w-full" alt={`Captura del proyecto ${project.title}`} />
-                  </a>
-                </div>
-              ))
-            }
-            {/* <div className="md:inline-block">
-              <a href="https://valoraprime.vercel.app/" target="_blank" rel="noopener noreferrer" aria-label="Abrir sitio del proyecto Valora Prime"
-                className="block rounded-2xl border-2 border-light-text/10 w-full h-20 md:w-24 md:h-16 overflow-hidden hover:scale-105 hover:shadow-lg hover:border-theme-primary transition-transform duration-300">
-                <img src="/projects/valoraprime_01.webp" className="object-cover h-full w-full" alt="Captura del proyecto Valora Prime" />
-              </a>
-            </div>
-            <div className="md:inline-block"> <a href="https://github.com/itsteddydev/notepad" target="_blank"
-              rel="noopener noreferrer" aria-label="Abrir sitio del proyecto Notepad"
-              className="block rounded-2xl border-2 border-light-text/10 w-full h-20 md:w-24 md:h-16 overflow-hidden hover:scale-105 hover:shadow-lg hover:border-theme-primary transition-transform duration-300">
-              <img src="/projects/notepad_01.webp" className="object-cover h-full w-full"
-                alt="Captura del proyecto Notepad" /> </a> </div>
-            <div className="md:inline-block"> <a href="https://github.com/itsteddydev/neko-inn" target="_blank"
-              rel="noopener noreferrer" aria-label="Abrir sitio del proyecto Neko Inn"
-              className="block rounded-2xl border-2 border-light-text/10 w-full h-20 md:w-24 md:h-16 overflow-hidden hover:scale-105 hover:shadow-lg hover:border-theme-primary transition-transform duration-300">
-              <img src="/projects/neko_inn_01.webp" className="object-cover h-full w-full"
-                alt="Captura del proyecto Neko Inn" /> </a> </div>
-            <div className="md:inline-block"> <a
-              href="https://astro.build/themes/details/personal-hub-a-customizable-personal-link/" target="_blank"
-              rel="noopener noreferrer" aria-label="Abrir sitio del proyecto Personal Hub"
-              className="block rounded-2xl border-2 border-light-text/10 w-full h-20 md:w-24 md:h-16 overflow-hidden hover:scale-105 hover:shadow-lg hover:border-theme-primary transition-transform duration-300">
-              <img src="/projects/personal_hub.webp" className="object-cover h-full w-full"
-                alt="Captura del proyecto Personal Hub" /> </a> </div> */}
+          <div className="relative mt-4 h-28 md:h-42 overflow-hidden">
+            <Marquee pauseOnHover className="[--duration:20s]">
+              {
+                projects.slice(0, 4).map((project, index) => (
+                  <div key={index} className="md:inline-block">
+                    <a href={project.link ? project.link : project.github} target="_blank" rel="noopener noreferrer" aria-label={`${project.title}`}
+                      className="block rounded-2xl w-full h-28 md:w-44 md:h-32 overflow-hidden hover:scale-105 hover:shadow-lg hover:border-theme-primary transition-transform duration-300">
+                      <img src={project.img} className="object-cover h-full w-full" alt={`${project.title}`} />
+                    </a>
+                  </div>
+                ))
+              }
+            </Marquee>
+            <div className="from-background pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-linear-to-r"></div>
+            <div className="from-background pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-linear-to-l"></div>
           </div>
           <Link
             href="/projects"
-            className={`absolute -bottom-3 left-1/2 transform -translate-x-1/2 w-fit flex items-center gap-2 bg-slate-400/20 font-medium text-xs text-white rounded p-2 shadow-lg transition-opacity duration-500 hover:bg-primary/90 group`}
+            className={`absolute bottom-2 xl:-bottom-3 left-1/2 transform -translate-x-1/2 w-fit flex items-center gap-2 bg-slate-400/20 font-medium text-xs text-white rounded p-2 px-3 shadow-lg transition-opacity duration-500 hover:bg-primary/90 group`}
+          >
+            View More Projects
+            <FaAnglesRight className="transform transition-transform duration-300 group-hover:translate-x-1" />
+          </Link>
+        </div>
+      </div>
+      <div className="block md:hidden xl:col-span-2 xl:row-span-4 xl:col-start-6 rounded-2xl border-2 border-light-text/10">
+        <div className="pt-4 pb-2 relative">
+          <div className="border-b-2 border-light-text/10 pb-2">
+            <IconBadge
+              icon={<GrProjects className="text-primary" />}
+              text={"Projects"}
+              className="py-2 font-medium"
+              iconSize={18}
+            />
+            <p className="text-sm text-neutral-400">Works diuh</p>
+          </div>
+          <div className="relative mt-4 h-42 overflow-hidden">
+            <Marquee pauseOnHover className="[--duration:20s]">
+              {
+                projects.slice(0, 4).map((project, index) => (
+                  <div key={index} className="md:inline-block">
+                    <a href={project.link ? project.link : project.github} target="_blank" rel="noopener noreferrer" aria-label={`${project.title}`}
+                      className="block rounded-2xl w-full h-28 md:w-44 md:h-32 overflow-hidden hover:scale-105 hover:shadow-lg hover:border-theme-primary transition-transform duration-300">
+                      <img src={project.img} className="object-cover h-full w-full" alt={`${project.title}`} />
+                    </a>
+                  </div>
+                ))
+              }
+            </Marquee>
+            <div className="from-background pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-linear-to-r"></div>
+            <div className="from-background pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-linear-to-l"></div>
+          </div>
+          <Link
+            href="/projects"
+            className={`absolute bottom-3 left-1/2 transform -translate-x-1/2 w-fit flex items-center gap-2 bg-slate-400/20 font-medium text-xs text-white rounded p-2 px-3 shadow-lg transition-opacity duration-500 hover:bg-primary/90 group`}
           >
             View More Projects
             <FaAnglesRight className="transform transition-transform duration-300 group-hover:translate-x-1" />
--- data/index.tsx
+++ data/index.tsx
@@ -1,92 +1,96 @@
+export const socials = {
+  mail: "mailto:keerthanns091@gmail.com"
+}
+
 export const experiences = [
-    {
-        title: "Product Engineer",
-        company: "Edgeverve Systems (Infosys finacle)",
-        location: "Bangalore, Karnataka",
-        website: "https://www.edgeverve.com/",
-        logo: "finacle.png",
-        period: "Aug 2024- Present",
-        description: `• Gained hands-on experience in UI/UX development using ReactJS, Typescript, HTML5, CSS, JS
+  {
+    title: "Product Engineer",
+    company: "Edgeverve Systems (Infosys finacle)",
+    location: "Bangalore, Karnataka",
+    website: "https://www.edgeverve.com/",
+    logo: "finacle.png",
+    period: "Aug 2024- Present",
+    description: `• Gained hands-on experience in UI/UX development using ReactJS, Typescript, HTML5, CSS, JS
             • Worked on DevOps and deployment automation using Jenkins, Docker, and Kubernetes`,
-    }, {
-        title: "Product Engineer Intern",
-        company: "Edgeverve Systems",
-        location: "Bangalore, Karnataka",
-        website: "https://www.edgeverve.com/",
-        logo: "edgeverve.png",
-        period: "Feb 2024- Jul 2024",
-        description: `• Worked on Spring batch framework to improve the performance of existing batch jobs.`,
-    }, {
-        title: "Software Developer Intern",
-        company: "Inspirante Technology Pvt. Ltd",
-        location: "Udupi, Karnataka",
-        website: "https://inspirantech.in/",
-        logo: "inspirante.jpeg",
-        period: "Aug 2022-Feb 2024",
-        description: `• Gained experience in UI development using NextJS, Typescript along with Prisma ORM and GraphQL. 
+  }, {
+    title: "Product Engineer Intern",
+    company: "Edgeverve Systems",
+    location: "Bangalore, Karnataka",
+    website: "https://www.edgeverve.com/",
+    logo: "edgeverve.png",
+    period: "Feb 2024- Jul 2024",
+    description: `• Worked on Spring batch framework to improve the performance of existing batch jobs.`,
+  }, {
+    title: "Software Developer Intern",
+    company: "Inspirante Technology Pvt. Ltd",
+    location: "Udupi, Karnataka",
+    website: "https://inspirantech.in/",
+    logo: "inspirante.jpeg",
+    period: "Aug 2022-Feb 2024",
+    description: `• Gained experience in UI development using NextJS, Typescript along with Prisma ORM and GraphQL. 
             • Developed a full- stack web application for Anugraha coaching using t3 stack.
             • Revamped the company website and Abhijnya services website using a modern theme (NextJS).`,
-    }, {
-        title: "Web Development Intern",
-        company: "Vitvara Technology",
-        location: "Mangalore, Karnataka",
-        website: "https://www.vitvara.in/",
-        logo: "vitvara.png",
-        period: "Aug 2021-Sept 2021",
-        description: `• Developed a responsive website for a local business using HTML5, CSS, and JavaScript.`,
-    }
+  }, {
+    title: "Web Development Intern",
+    company: "Vitvara Technology",
+    location: "Mangalore, Karnataka",
+    website: "https://www.vitvara.in/",
+    logo: "vitvara.png",
+    period: "Aug 2021-Sept 2021",
+    description: `• Developed a responsive website for a local business using HTML5, CSS, and JavaScript.`,
+  }
 ]
 
 export const projects = [
-      {
-        title: 'Incridea-2023',
-        description: 'Website for the annual techno-cultural fest of NMAMIT, called Incridea. Worked in team of 9. The website was visited by over 5000+ people throughout the duration of the fest.',
-        github: 'https://github.com/incridea-23/incridea-client',
-        img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1683852443/portfolioProjects/uqau9y2uttv5ma9wjqa1.png",
-        tags: ['Nextjs','TypeScript', 'TailwindCSS', 'GraphQl','Prisma']
-      },{
-        title: 'Incridea-2024',
-        description: 'Website for the annual techno-cultural fest of NMAMIT, called Incridea. Worked in team of 24. The website was visited by over 5000+ people throughout the duration of the fest.',
-        github: 'https://github.com/incridea-23/incridea-client',
-        link: 'http://incridea.in',
-        img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1717234994/portfolioProjects/nuasdlmme1hwy6n8evki.png",
-        tags: ['Nextjs','TypeScript', 'TailwindCSS', 'GraphQl','Prisma']
-      },
-      {
-        title: 'Anugraha coaching',
-        description: 'Anugraha Online Coaching Center is built with the T3 stack. It supports English and Kannada languages. Students can submit details and payment proof, which the admin can verify and manage. Automatic mail confirmation feature.',
-        link: 'https://anugraha.vercel.app',
-        img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1705248952/portfolioProjects/anugraha_o1932k.png",
-        tags: ['NextJS','Typescript','Tailwindcss','Prisma','tRPC','NextAuth','Supabase']
-      },
-      {
-        title: 'Inspirante Technologies',
-        description: 'Inspirante Technologies Pvt Ltd provides Software-as-a-Service [SAAS] ERP solutions. I have revamped the website by adding recaptcha, auto-mail system features and validations.',
-        link: 'https://inspirantech.in/',
-        img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1705248981/portfolioProjects/inspirantech_opvkzj.png",
-        tags: ['Html','css','Bootstrap','Javascript','Php']
-      },
-      {
-        title: 'Abhijinya Education',
-        description: 'A static website built while an web development intern in Inspirante Technologies Pvt.Ltd. Abhijinya Education Services offers Aptitude training, Soft skills, Workshops, Seminars, Job preparation techniques & Personality development programs.',
-        link: 'https://abhijnyaeducation.com/',
-        img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1683852442/portfolioProjects/udibcop7esvui5bylgy8.png",
-        tags: ['html', 'css/scss',' Bootstrap']
-      },
-      {
-        title: 'My Portfolio (This website)',
-        description: 'This is my portfolio website. It is a static website built using ReactJS and TailwindCSS. I really enjoyed building this website.',
-        github: 'https://github.com/keerthan-ns/portfolio',
-        link: 'https://keerthanns.vercel.app/',
-        img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1688752957/portfolioProjects/portPro_b6lsrl.png",
-        tags: ['ReactJS','css','TailwindCSS']
-      },
-      {
-        title: 'VChat',
-        description: 'The Vchat(Instagram clone) is a user-friendly web-application. The website has a modern, responsive design, and uses a combination of technologies such as ReactJs and MongoDB to provide a smooth and seamless user experience.',
-        github: 'https://github.com/keerthan-ns/Vchat',
-        link: 'https://vchat-now.vercel.app/',
-        img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1683852442/portfolioProjects/wxp6qwy2qw9beyygziog.png",
-        tags: ['ReactJS','css/scss','Django','MongoDB']
-      }
-    ]
+  {
+    title: 'Incridea-2023',
+    description: 'Website for the annual techno-cultural fest of NMAMIT, called Incridea. Worked in team of 9. The website was visited by over 5000+ people throughout the duration of the fest.',
+    github: 'https://github.com/incridea-23/incridea-client',
+    img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1683852443/portfolioProjects/uqau9y2uttv5ma9wjqa1.png",
+    tags: ['Nextjs', 'TypeScript', 'TailwindCSS', 'GraphQl', 'Prisma']
+  }, {
+    title: 'Incridea-2024',
+    description: 'Website for the annual techno-cultural fest of NMAMIT, called Incridea. Worked in team of 24. The website was visited by over 5000+ people throughout the duration of the fest.',
+    github: 'https://github.com/incridea-23/incridea-client',
+    link: 'http://incridea.in',
+    img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1717234994/portfolioProjects/nuasdlmme1hwy6n8evki.png",
+    tags: ['Nextjs', 'TypeScript', 'TailwindCSS', 'GraphQl', 'Prisma']
+  },
+  {
+    title: 'Anugraha coaching',
+    description: 'Anugraha Online Coaching Center is built with the T3 stack. It supports English and Kannada languages. Students can submit details and payment proof, which the admin can verify and manage. Automatic mail confirmation feature.',
+    link: 'https://anugraha.vercel.app',
+    img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1705248952/portfolioProjects/anugraha_o1932k.png",
+    tags: ['NextJS', 'Typescript', 'Tailwindcss', 'Prisma', 'tRPC', 'NextAuth', 'Supabase']
+  },
+  {
+    title: 'Inspirante Technologies',
+    description: 'Inspirante Technologies Pvt Ltd provides Software-as-a-Service [SAAS] ERP solutions. I have revamped the website by adding recaptcha, auto-mail system features and validations.',
+    link: 'https://inspirantech.in/',
+    img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1705248981/portfolioProjects/inspirantech_opvkzj.png",
+    tags: ['Html', 'css', 'Bootstrap', 'Javascript', 'Php']
+  },
+  {
+    title: 'Abhijinya Education',
+    description: 'A static website built while an web development intern in Inspirante Technologies Pvt.Ltd. Abhijinya Education Services offers Aptitude training, Soft skills, Workshops, Seminars, Job preparation techniques & Personality development programs.',
+    link: 'https://abhijnyaeducation.com/',
+    img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1683852442/portfolioProjects/udibcop7esvui5bylgy8.png",
+    tags: ['html', 'css/scss', ' Bootstrap']
+  },
+  {
+    title: 'My Portfolio (This website)',
+    description: 'This is my portfolio website. It is a static website built using ReactJS and TailwindCSS. I really enjoyed building this website.',
+    github: 'https://github.com/keerthan-ns/portfolio',
+    link: 'https://keerthanns.vercel.app/',
+    img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1688752957/portfolioProjects/portPro_b6lsrl.png",
+    tags: ['ReactJS', 'css', 'TailwindCSS']
+  },
+  {
+    title: 'VChat',
+    description: 'The Vchat(Instagram clone) is a user-friendly web-application. The website has a modern, responsive design, and uses a combination of technologies such as ReactJs and MongoDB to provide a smooth and seamless user experience.',
+    github: 'https://github.com/keerthan-ns/Vchat',
+    link: 'https://vchat-now.vercel.app/',
+    img: "https://res.cloudinary.com/dg7etzwks/image/upload/v1683852442/portfolioProjects/wxp6qwy2qw9beyygziog.png",
+    tags: ['ReactJS', 'css/scss', 'Django', 'MongoDB']
+  }
+]
