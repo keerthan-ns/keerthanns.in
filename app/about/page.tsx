3d0b0c6adab601ca94fc64ab136a1e922b1880e8
Keerthan N S <keerthanns091@gmail.com>
2025-10-26 18:29:53 +0530

    [update] portfolio metadata [del] unused icons [add] about & work pages [stable]

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import PageLayout from "../layout/PageLayout";
+
+
+export default function AboutPage() {
+  return (
+    <PageLayout>
+        <p>about page</p>
+    </PageLayout>
+  );
+}
---
+++ app/work/page.tsx
@@ -0,0 +1,10 @@
+import PageLayout from "../layout/PageLayout";
+
+
+export default function WorkPage() {
+  return (
+    <PageLayout>
+        <p>work page</p>
+    </PageLayout>
+  );
+}
