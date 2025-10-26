8243fe218825218cbf6d7330a4a1f2ce644b0f95
Keerthan N S <keerthanns091@gmail.com>
2025-10-26 17:37:47 +0530

    [add] floating button for command palette [stable]

--- components/cards/FooterCard.tsx
+++ components/cards/FooterCard.tsx
@@ -4,7 +4,7 @@ export default function FooterCard() {
     return (
         <Fragment>
             <div className="hidden xl:flex flex-col justify-center xl:col-span-3 xl:row-span-1 xl:col-start-3 xl:row-start-7 p-2 text-sm font-light rounded-2xl border-2 border-light-text/10">
-                <div>Press <kbd className="opacity-50 bg-slate-500/30 p-1 rounded">Ctrl</kbd> + <kbd className="opacity-50 bg-slate-500/30 p-1 rounded">K</kbd> to open the command palette.</div>
+                <div>Press <kbd className="opacity-50 bg-slate-500/30 p-1 rounded">Ctrl</kbd> + <kbd className="opacity-50 bg-slate-500/30 p-1 rounded">K</kbd> to open the command palette</div>
                 <p className="text-sm font-medium text-slate-200">
                     &copy; {new Date().getFullYear()} {'Keerthan N S'}
                 </p>
--- components/ui/CommandPalette.tsx
+++ components/ui/CommandPalette.tsx
@@ -15,6 +15,7 @@ import { socials } from "@/data";
 import { BsGithub, BsInstagram, BsLinkedin, BsTwitterX } from "react-icons/bs";
 import { FaAnglesRight } from "react-icons/fa6";
 import { IoDocumentText } from "react-icons/io5";
+import { RiCommandLine } from "react-icons/ri";
 
 type CommandLink = {
     group: string;
@@ -134,61 +135,68 @@ export default function CommandPalette() {
     const groups = Array.from(new Set(commands.map((c) => c.group)));
 
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogContent className="p-0 shadow-lg max-w-lg overflow-hidden bg-[#171717]">
-                <Command>
-                    <CommandInput placeholder="Search command..." />
-                    <CommandList>
-                        <CommandEmpty>No results found.</CommandEmpty>
+        <Fragment>
+            <button onClick={() => setOpen(true)}
+                className="cursor-pointer fixed bottom-6 right-6 z-50 w-14 h-14 rounded-full bg-primary/50 text-white shadow-lg flex items-center justify-center hover:bg-primary/90 transition-all duration-300"
+                aria-label="Open Command Palette">
+                <RiCommandLine size={24} />
+            </button>
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent className="p-0 shadow-lg max-w-lg overflow-hidden bg-[#171717]">
+                    <Command>
+                        <CommandInput placeholder="Search command..." />
+                        <CommandList>
+                            <CommandEmpty>No results found.</CommandEmpty>
 
-                        {groups.map((gName, idx) => {
-                            return (
-                                <Fragment key={gName}>
-                                    <CommandGroup heading={gName}>
-                                        {commands
-                                            .filter((c) => c.group === gName)
-                                            .map((cmd, i) => (
-                                                <CommandItem
-                                                    key={i}
-                                                    onSelect={() => {
-                                                        if (cmd.type === "link") {
-                                                            if (cmd.newTab)
-                                                                window.open(cmd.href, "_blank");
-                                                            else window.location.href = cmd.href;
-                                                        } else if (cmd.type === "fn") {
-                                                            cmd.fn();
-                                                        }
-                                                        setOpen(false);
-                                                    }}
-                                                >
-                                                    {cmd.icon}
-                                                    <span>{cmd.label}</span>
-                                                    {cmd.shortcut && (
-                                                        <kbd className="ml-auto text-xs opacity-50">
-                                                            {cmd.shortcut}
-                                                        </kbd>
-                                                    )}
-                                                </CommandItem>
-                                            ))}
-                                    </CommandGroup>
-                                    {idx < groups.length - 1 && <CommandSeparator />}
-                                </Fragment>
-                            );
-                        })}
-                    </CommandList>
-                </Command>
-                <div className="flex flex-row justify-between p-2 border-t border-t-slate-700 text-slate-300 text-sm">
-                    <p>
-                        <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">↩</kbd> to select
-                    </p>
-                    <p>
-                        <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">↑</kbd> <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">↓</kbd> to navigate
-                    </p>
-                    <p>
-                        <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">Ctrl K</kbd> / <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">Escape</kbd> to close
-                    </p>
-                </div>
-            </DialogContent>
-        </Dialog>
+                            {groups.map((gName, idx) => {
+                                return (
+                                    <Fragment key={gName}>
+                                        <CommandGroup heading={gName}>
+                                            {commands
+                                                .filter((c) => c.group === gName)
+                                                .map((cmd, i) => (
+                                                    <CommandItem
+                                                        key={i}
+                                                        onSelect={() => {
+                                                            if (cmd.type === "link") {
+                                                                if (cmd.newTab)
+                                                                    window.open(cmd.href, "_blank");
+                                                                else window.location.href = cmd.href;
+                                                            } else if (cmd.type === "fn") {
+                                                                cmd.fn();
+                                                            }
+                                                            setOpen(false);
+                                                        }}
+                                                    >
+                                                        {cmd.icon}
+                                                        <span>{cmd.label}</span>
+                                                        {cmd.shortcut && (
+                                                            <kbd className="ml-auto text-xs opacity-50">
+                                                                {cmd.shortcut}
+                                                            </kbd>
+                                                        )}
+                                                    </CommandItem>
+                                                ))}
+                                        </CommandGroup>
+                                        {idx < groups.length - 1 && <CommandSeparator />}
+                                    </Fragment>
+                                );
+                            })}
+                        </CommandList>
+                    </Command>
+                    <div className="flex flex-row justify-between p-2 border-t border-t-slate-700 text-slate-300 text-sm">
+                        <p>
+                            <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">↩</kbd> to select
+                        </p>
+                        <p>
+                            <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">↑</kbd> <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">↓</kbd> to navigate
+                        </p>
+                        <p>
+                            <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">Ctrl K</kbd> / <kbd className="text-sm opacity-50 bg-slate-500/30 p-1 rounded">Escape</kbd> to close
+                        </p>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </Fragment>
     );
 }
