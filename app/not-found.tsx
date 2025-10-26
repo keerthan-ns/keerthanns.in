689ee72a83190ba29efaecb9acf11b7baf7e9fcb
Keerthan N S <keerthanns091@gmail.com>
2025-10-26 18:09:01 +0530

    [fix] padding issue

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center h-screen text-white">
       <h1 className="text-6xl font-bold mb-4 text-primary">404</h1>
       <p className="text-2xl mb-8">Oops! You seem to be lost.</p>
-      <div>
+      <div className="text-center px-3">
         Click on the <span className="text-primary">Command Palette</span> below
         (<kbd className="opacity-50 bg-slate-500/30 p-1 rounded">Ctrl</kbd>+<kbd className="opacity-50 bg-slate-500/30 p-1 rounded">K</kbd>{" "})
         and navigate to other pages.
