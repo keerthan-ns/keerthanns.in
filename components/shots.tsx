384f054142d4a86f2d503cec2ee6d0f8da86df4c
keerthan ns <keerthanns091@gmail.com>
2025-10-29 19:38:34 +0530

    [fix] shots-tab mobile view [stable]

--- components/shots.tsx
+++ components/shots.tsx
@@ -1,12 +1,7 @@
 
 export function Shots() {
   return (
-    <div
-      className="
-        hidden md:block xl:col-span-2 xl:row-span-3 xl:col-start-6
-        relative overflow-hidden bg-[url('/static/others/grid.png')] bg-cover bg-center h-screen
-      "
-    >
+    <div className="relative overflow-hidden bg-[url('/static/others/grid.png')] bg-cover bg-center h-screen">
       <div className="absolute inset-0 flex items-center justify-center">
         <p className="text-2xl font-normal">
           Something&apos;s Coming Soon!
